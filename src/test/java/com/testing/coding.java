62d9a3a91ff9e786dd7ddbabedf20520e8119556
Myselfdass <jesudas424@gmail.com>
2024-01-31 14:39:59 +0530

    final commit

---
+++ src/test/java/com/testing/coding.java
@@ -0,0 +1,28 @@
+package com.testing;
+
+public class coding {
+
+	public static void main(String[] args) {
+
+		String S2 = "India.Srilanka.Pakistan";
+		
+		String data[]=S2.split("\\.");
+		
+		for(int i=0; i<data.length; i++) {
+			String value = data[i];
+			System.out.println(value);
+			
+	}		
+		
+		String str = " My name is Dass";
+		String[] split = str.split(" ");
+		for(int i=0; i<split.length; i++) {
+			String du=split[i];
+			System.out.println(du);
+		}
+		
+		
+	}
+}
+
+
